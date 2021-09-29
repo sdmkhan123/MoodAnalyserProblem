deb348de17b858c821106cd8c2e0c19f7e4ad4c7
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-29 21:02:45 +0530

    To check mood is SAD or HAPPY

---
+++ MoodAnalyser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoodAnalyserProblem
+{
+    public class MoodAnalyser
+    {
+        public string toCheckMood(string str)
+        {
+            string[] mood = str.Split(" ");
+            for(int i=0; i<mood.Length; i++)
+            {
+                if (mood[i].ToLower() == "sad")
+                {
+                    return "SAD";
+                }
+            }
+            return "Happy";
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,6 +9,11 @@ namespace MoodAnalyserProblem
             Console.WriteLine("***************************************************************************\n");
             Console.WriteLine("Welcome to Mood Analyser Problem");
             Console.WriteLine("\n***************************************************************************\n");
+            MoodAnalyser moodAnalyser = new MoodAnalyser();
+            Console.WriteLine("Write about your mood");
+            string mood = Console.ReadLine();
+            Console.WriteLine(moodAnalyser.toCheckMood(mood));
+            Console.WriteLine("\n***************************************************************************\n");
         }
     }
 }
