c4bf54f38df91b9d8a42c8ac6b3786e8d4d63862
sdmkhan123 <sdmkhan126410@gmail.com>
2021-09-29 14:48:59 +0530

    Welcome to Mood Analyser

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MoodAnalyserProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***************************************************************************\n");
+            Console.WriteLine("Welcome to Mood Analyser Problem");
+            Console.WriteLine("\n***************************************************************************\n");
+        }
+    }
+}
